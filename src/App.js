afb934fcbc00aa09cb46856ad2b35d4344a28024
Sam Keathley <samncollette@gmail.com>
2020-06-24 15:06:40 -0700

    removed styled-components in favor of pure css and js dark mode solution

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import { useDarkMode } from './useDarkMode';
-import { lightTheme, darkTheme } from './theme';
-import { GlobalStyles } from './global';
-import Toggle from './Toggle';
+import DarkMode from './DarkMode';
 
 function App() {
-  const [theme, toggleTheme, componentMounted] = useDarkMode();
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
-
-  if (!componentMounted) {
-    return <div />
-  }
-
   return (
-    <ThemeProvider theme={themeMode}>
-      <>
-        <GlobalStyles />
-        <Toggle theme={theme} toggleTheme={toggleTheme} />
-        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
-        <footer>
-        </footer>
-      </>
-    </ThemeProvider>
+    <div>
+      <DarkMode />
+    </div>
   );
 }
 
---
+++ src/DarkMode.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+function DarkMode() {
+    const [darkMode, setDarkMode] = useState(getInitialMode);
+    useEffect(() => {
+        localStorage.setItem("dark", JSON.stringify(darkMode));
+    }, [darkMode]);
+
+    function getInitialMode() {
+        const isReturningUser = "dark" in localStorage;
+        const savedMode = JSON.parse(localStorage.getItem("dark"));
+        const userPrefDark = getPrefColorScheme();
+        if (isReturningUser) {
+            return savedMode;
+        } else if (userPrefDark) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function getPrefColorScheme() {
+        if (!window.matchMedia) return;
+
+        return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+
+    return (
+        <div className={darkMode ? "dark-mode" : "light-mode"}>
+            <nav>
+                <div className="toggle-container">
+                    <span role="img" aria-label="Sun" style={{ color: darkMode ? "grey" : "yellow" }}>
+                        ☀️
+                    </span>
+                    <span className="toggle">
+                        <input
+                            checked={darkMode}
+                            onChange={() => setDarkMode(prevMode => !prevMode)}
+                            type="checkbox"
+                            className="checkbox"
+                            id="checkbox"
+                        />
+                        <label htmlFor="checkbox" />
+                    </span>
+                    <span role="img" aria-label="Moon" style={{ color: darkMode ? "slateblue" : "grey" }}>
+                        🌙
+                    </span>
+                </div>
+            </nav>
+            <main>
+                <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+                <h2>Toggle the switch to see some magic happen!</h2>
+            </main>
+        </div>
+    );
+}
+
+export default DarkMode;
